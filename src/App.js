e1e3e2d648d278d634b05c364dbfbf73d22461bd
eNViDAT-student <nvdat.ngvan@gmail.com>
2022-09-18 09:12:55 +0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header1";
+import Header from "./components/Header/Header";
 import MainRoute from "./routing/MainRoute";
 import "./App.css";
 function App() {

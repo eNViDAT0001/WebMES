d86dcfb44dd170a14741614fed8d531955b9c0dc
khanhsd0901 <khanhsd0901@gmail.com>
2022-09-18 10:10:29 +0700

    full containerboxOption in Category

--- src/pages/CategoryPages/CategoryPages.js
+++ src/pages/CategoryPages/CategoryPages.js
@@ -1,34 +1,40 @@
 
+const HeaderBarContainer = "w-full h-47px bg-gray-header"
+const HeaderFontContainer = "flex justify-start items-center flex-row flex-nowrap py-14px px-183px font-medium text-base"
+const FullContainerOption = "w-1171px h-11 mt-6 mb-100px  flex justify-start flex-nowrap whitespace-nowrap	" //include option anh label
+const ContainerOnlyOption = "ml-200px pt-2 flex items-center"
 
 const CategoryPage = () =>{
     return (
+
     <div className=" flex-auto" >
-        <div className="w-full h-47px bg-gray-header ">
-            <div className="py-14px px-375px ">
-                <div className = "font-medium text-base">
-                    <div className="flex justify-start items-center flex-row flex-nowrap "> 
-                        <h1>Home.Product</h1>
-                        <h1 className="font-medium text-pink-500">.Ecommerce Acceories & Fashion item</h1>
-                    </div>
+
+            <div className={HeaderBarContainer}>
+                <div className={HeaderFontContainer}>
+                    <h1>Home.Product</h1>
+                    <h1 className="text-pink-500">.Ecommerce Acceories & Fashion item</h1>
                 </div>
-            </div>
+  
         </div>
-        <div className="w-full h-11 px-375px py-6">
-            <div className="flex justify-start flex-nowrap">
+        <div className="flex justify-center ">
+            <div className={FullContainerOption}>
                 <div className="flex flex-col">
                     <h1 className=" font-normal text-2xl text-purple-text">Ecommerce Acceories & Fashion item</h1>
-                    <h1 className= "font-normal text-xs self-start pt-2"> About 9,620 results (0.62 seconds)</h1>
+                    <h1 className= "font-normal text-xs self-start mt-2"> About 9,620 results (0.62 seconds)</h1>
                 </div>
-                <div className=" pl-200px flex items-center">
-                    <h1 className="font-normal text-purple-text-2 pr-2 ">Perpage</h1>
+                <div className={ContainerOnlyOption}>
+                    <h1 className="font-normal text-purple-text-2 mr-2 ">Perpage:</h1>
                     <input type = "text" className=" w-55px h-25px border " ></input>
-                    <h1 className="font-normal text-purple-text-2  "> Sort by:</h1>
+                    <h1 className="font-normal text-purple-text-2 ml-27px"> Sort by:</h1>
+                    <select className=" w-24 h-7 ml-2 border text-center text-xs text-gray-text-in-select" >
+                        <option value = "0" >Best match</option>
+                    </select>
+                    <h1 className="font-normal text-purple-text-2 pl-6">View:</h1>
+                    <input type = "text" className=" ml-60px w-40 border"></input>
                 </div>
             </div>
         </div>
-        <div className="w-full px-375px py-20">
-            <h1>Khanh</h1>
-        </div>
+
     </div>
     )
     
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,25 @@ module.exports = {
         '1171px': '1171px'
       },
       spacing: {
+        '5px': '5px',
+        '11px': '11px',
         '10px': '10px',
         '14px': '14px',
         '25px': '25px',
+        '27px': '27px',
         '47px': '47px',
         '55px': '55px',
+        '60px': '60px',
+        '100px': '100px',
+        '183px': '183px',
         '200px':'200px',
         '375px': '375px',
+        '1171px': '1171px',
       },
       colors: {
         'gray-header': '#F6F5FF',//gray
         'gray-input' : '#E7E6EF', //gray
+        'gray-text-in-select': "#8A8FB9",//gray
         'purple-text': '#151875',//purple
         'purple-text-2': '#3F509E'//purple
       },

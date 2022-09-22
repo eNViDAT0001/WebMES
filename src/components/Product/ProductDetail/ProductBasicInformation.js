8ccf83efc78f98f8734432ab2967b6c6a92a47d4
khanhsd0901 <khanhsd0901@gmail.com>
2022-09-22 09:03:32 +0700

    lat lam tiep

--- src/components/Product/ProductDetail/ProductBasicInformation.js
+++ src/components/Product/ProductDetail/ProductBasicInformation.js
@@ -1,3 +1,8 @@
+import Rating from "react-rating"
+import starActive from '../../../assets/star.png'
+import starNotActive from '../../../assets/star_not.png'
+
+
 const ProductBasicInformation = () =>{
     return(
         <div className="mt-45px w-1152px h-487px border flex flex-row">
@@ -5,8 +10,26 @@ const ProductBasicInformation = () =>{
             
             </div>
             <div className="ml-11 mt-61px">
-                <h1>Play</h1>
-            </div>  
+                <h1 className=" font-normal text-4xl text-purple-text-name-product-detail ">Playwood arm chair</h1>
+                <div className="flex flex-row mt-2">
+                    <Rating  
+                        emptySymbol={<img src={starNotActive} alt="starDisable" className="w-3 h-3 mr-1" />}
+                        fullSymbol={<img src={starActive} alt="star" className="w-3 h-3 mr-1" />}
+                        initialRating = {4}
+                        readonly = {true}/>
+                    <h1 className="text-purple-text capitalize text-sm "> (22) </h1> 
+                </div>
+                <div className="flex flex-row mt-14px text-base">
+                    <h1 className="mr-18px text-purple-text ">$32.00</h1>
+                    <h1 className=" text-pink-price-sale line-through">$32.00</h1>
+                </div>
+                <h1 className="mt-3 text-base text-purple-type-product-detail">Type</h1>
+
+                 
+            </div>
+            
+            
+            
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
         '10px': '10px',
         '14px': '14px',
         '17px': '17px',
+        '18px': '18px',
         '25px': '25px',
         '27px': '27px',
         '45px': '45px',
@@ -23,6 +24,7 @@ module.exports = {
         '55px': '55px',
         '60px': '60px',
         '61px': '61px',
+        '69px': '69px',
         '100px': '100px',
         '183px': '183px',
         '200px':'200px',
@@ -45,9 +47,12 @@ module.exports = {
         'purple-text': '#151875',//purple
         'purple-text-2': '#3F509E',//purple
         'purple-name-product': '#111C85',
+        'purple-type-product-detail': '#0D134E',
         'purple-check-product-brand': '#603EFF',
         'yellow-check-rating-item': '#FFCC2E',
         'pink-price-sale': ' #FF2AAA',
+        //PRODUCT DETAIL
+        'purple-text-name-product-detail': '#0D134E',
 
       },
       fontFamily:{

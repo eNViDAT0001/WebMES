37bc449e469f8a4b131b25dfc163dc5e81f7e6c0
khanhsd0901 <khanhsd0901@gmail.com>
2022-10-13 23:32:54 +0700

    add FAQ and Contact screen

--- src/pages/StorePages/ContactPage.js
+++ src/pages/StorePages/ContactPage.js
@@ -1,17 +1,17 @@
 import ContactHeaderBar from "../../components/StoreOtherComponent/Contact/ContactHeaderBar";
 import contact from '../../components/StoreOtherComponent/Contact/asset/contact.svg'
 import imgContact from '../../components/StoreOtherComponent/Contact/asset/imgContact.svg'
-
+import FormContact from "../../components/StoreOtherComponent/Contact/FormContact";
 const ContactPage = () =>{
     return (
-        <div className="flex flex-col ">
+        <div className="flex flex-col font-['Josefin_Sans']">
             <ContactHeaderBar />
-            <div className="flex justify-center">
+            <div className="flex justify-center mt-[120px]">
                 <div className="w-[65%] flex justify-center flex-col">
                     <img src={contact} alt="thong tin contact" ></img>
                     <div className="flex flex-row justify-between">
-                        
-                        <img src={imgContact} alt="anh minh hoa" className=" w-[80%] h-[80%] mr-[-15%] mt-[-15%]"></img>
+                        <FormContact />
+                        <img src={imgContact} alt="anh minh hoa" className=" w-[65%] h-[65%] mr-[-15%] mt-[-15%]"></img>
                     </div>
                     
                 </div>
--- src/pages/StorePages/FAQPage.js
+++ src/pages/StorePages/FAQPage.js
@@ -1,11 +1,21 @@
-
-import { Fragment } from "react";
+import GeneralInformation from "../../components/StoreOtherComponent/FAQ/GeneralInformation";
+import AskQuestion from "../../components/StoreOtherComponent/FAQ/AskQuestion";
+import FAQHeaderBar from "../../components/StoreOtherComponent/FAQ/FAQHeaderBar";
+import listLogo from '../../components/StoreOtherComponent/OrderCompleted/asset/listLogo.png'
 
 const FAQPage = () =>{
     return (
-        <Fragment>
-            <div>đây là faq</div>
-        </Fragment>
+        <div className="font-['Josefin_Sans']">
+            <FAQHeaderBar />
+            <div className="flex items-center flex-col">
+                <div className="w-[80%] min-h-[100px] h-[80%] max-h-[740px] mt-[130px] flex flex-row justify-between">
+                    <GeneralInformation />
+                    <AskQuestion />
+                </div>
+                <img className='w-[80%] h-[121px] my-[91px]' src={listLogo} alt= "anh logo"></img>
+            </div>
+            
+        </div>
     )
     
 };
